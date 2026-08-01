9ab0e5bc49ec304b365f2f3dc891b662a896d28b
Rob Ede <robjtede@icloud.com>
2026-08-01 20:59:24 +0100

    ci: use shared release comment tools

--- flake.nix
+++ flake.nix
@@ -3,19 +3,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    x52 = {
+      url = "github:x52dev/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
   };
 
   outputs = inputs @ { flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-      perSystem = { pkgs, config, ... }:
+      perSystem = { pkgs, config, inputs', ... }:
         {
           # Rust is managed outside Nix (e.g. rustup / CI setup action).
           # Keep the flake focused on auxiliary tooling.
           devShells.default = pkgs.mkShell {
             packages = [
               config.formatter
+              inputs'.x52.packages.x52-release-tools
               pkgs.cargo-shear
               pkgs.cargo-hack
               pkgs.fd
